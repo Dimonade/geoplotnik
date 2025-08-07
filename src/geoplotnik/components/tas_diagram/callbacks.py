ae3dd7601946dc005fe9e560f3e73f0b4829a6a8
Dmitry <dimonade@protonmail.com>
2025-08-07 19:36:59 +0200

    Add TAS diagram overlay with names (#17)
    * initial implementation.
    
    * chore - redestribute code and fix imports.
    
    * feature - add optional exact location of label, clean border polygons.
    
    * chore - adjust coordinates and label representation.

--- src/geoplotnik/components/tas_diagram/callbacks.py
+++ src/geoplotnik/components/tas_diagram/callbacks.py
@@ -1,8 +1,11 @@
 import base64
+from dataclasses import dataclass
+from enum import Enum
 from typing import Any
 
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from dash import callback
 from dash import dcc
 from dash import html
@@ -15,9 +18,9 @@ from geoplotnik.components.ids import DATA_UPLOAD_AREA
 from geoplotnik.components.ids import DATA_UPLOAD_PREVIEW
 from geoplotnik.components.ids import TAS_DIAGRAM
 from geoplotnik.components.ids import TAS_DIAGRAM_GROUPING_PARAMETER_DROPDOWN
-from geoplotnik.components.ids import TAS_DIAGRAM_GROUPING_PARAMETER_SELECT_ALL_BUTTON
 from geoplotnik.components.ids import TAS_DIAGRAM_X_AXIS_DROPDOWN
 from geoplotnik.components.ids import TAS_DIAGRAM_Y_AXIS_DROPDOWN
+from geoplotnik.components.tas_diagram.rocks import Rocks
 from geoplotnik.data.loaders import load_data
 
 
@@ -28,7 +31,9 @@ from geoplotnik.data.loaders import load_data
     State(DATA_UPLOAD_AREA, "filename"),
     State(DATA_UPLOAD_AREA, "last_modified"),
 )
-def update_data_store(url: str, contents: list, names: list, dates: list) -> dict:
+def update_data_store(
+    url: str, contents: list, names: list, dates: list
+) -> list[dict[str, Any]]:
     print("Updating data store.")
 
     # If this is true, we probably have an issue.
@@ -75,10 +80,9 @@ def update_data_preview(
     Output(TAS_DIAGRAM_Y_AXIS_DROPDOWN, "options"),
     Output(TAS_DIAGRAM_Y_AXIS_DROPDOWN, "value"),
     Input(DATA_STORE, "data"),
-    Input(TAS_DIAGRAM_GROUPING_PARAMETER_SELECT_ALL_BUTTON, "n_clicks"),
 )
 def update_grouping_parameter_dropdown(
-    data: list[dict[str, Any]], _: int
+    data: list[dict[str, Any]],
 ) -> tuple[
     list[dict[str, str]], str, list[dict[str, str]], str, list[dict[str, str]], str
 ]:
@@ -95,7 +99,7 @@ def update_grouping_parameter_dropdown(
         opts,
         opts[1]["value"],
         opts,
-        opts[2]["value"],
+        opts[-1]["value"],
     )
 
 
@@ -107,7 +111,7 @@ def update_grouping_parameter_dropdown(
     Input(TAS_DIAGRAM_GROUPING_PARAMETER_DROPDOWN, "value"),
 )
 def update_tas_diagram(
-    data: list[dict],
+    data: list[dict[str, Any]],
     x_axis: str,
     y_axis: str,
     grouping_parameter: str,
@@ -124,12 +128,8 @@ def update_tas_diagram(
     if not data:
         return html.Div("No data for selected locations.", id=TAS_DIAGRAM)
 
-    df = pd.DataFrame(data)
-
-    df[x_axis] = pd.to_numeric(df[x_axis], errors="coerce")
-    df[y_axis] = pd.to_numeric(df[y_axis], errors="coerce")
-
-    df = df.dropna(subset=[x_axis, y_axis])
+    df = load_data_from_store(data)
+    df = convert_to_numeric_axes(df, x_axis, y_axis)
 
     if len(df) == 0:
         raise PreventUpdate
@@ -137,10 +137,86 @@ def update_tas_diagram(
     print(f"Types in x_axis: {set([type(x) for x in df[x_axis]])}")
     print(f"TYpes in y axis: {set([type(y) for y in df[y_axis]])}")
 
-    group_colour = grouping_parameter if grouping_parameter in df.columns else None
-    print(f"Colour groups: {group_colour}")
+    group = grouping_parameter if grouping_parameter in df.columns else None
 
-    fig = px.scatter(df, x=x_axis, y=y_axis, color=group_colour)
-    fig.update_layout(title_text="TAS Diagram", title_x=0.5)
+    fig = px.scatter(df, x=x_axis, y=y_axis, color=group, symbol=group)
+    fig.update_layout(
+        title_text="TAS Diagram",
+        title_x=0.5,
+        height=800,
+        plot_bgcolor="white",
+        paper_bgcolor="white",
+        xaxis={"gridcolor": "black"},
+        yaxis={"gridcolor": "black"},
+    )
+    fig = create_tas_overlay(fig)
     print("Creating a TAS diagram.")
-    return html.Div(dcc.Graph(figure=fig), id=TAS_DIAGRAM)
+    return html.Div(
+        dcc.Graph(figure=fig),
+        id=TAS_DIAGRAM,
+    )
+
+
+def load_data_from_store(data_in: list[dict[str, Any]]) -> pd.DataFrame:
+    return pd.DataFrame(data_in)
+
+
+def convert_to_numeric_axes(df: pd.DataFrame, x_axis: str, y_axis: str) -> pd.DataFrame:
+    df[x_axis] = pd.to_numeric(df[x_axis], errors="coerce")
+    df[y_axis] = pd.to_numeric(df[y_axis], errors="coerce")
+
+    df = df.dropna(subset=[x_axis, y_axis])
+    return df
+
+
+def create_tas_overlay(fig: Any) -> Any:
+    for traces in Rocks.to_overlay_traces():
+        poly, label = traces
+        fig.add_trace(poly)
+        fig.add_trace(label)
+
+    # Each rock contributes 2 overlays: the polygon and the label.
+    num_overlay_traces = len(Rocks) * 2
+
+    fig.update_layout(
+        updatemenus=[
+            {
+                "type": "buttons",
+                "direction": "left",
+                "buttons": [
+                    {
+                        "label": "Toggle overlay",
+                        "method": "update",
+                        "args": [
+                            {"visible": [True] * len(fig.data)},
+                            {
+                                "title": {
+                                    "text": "TAS Diagram",
+                                    "x": 0.5,
+                                }
+                            },
+                        ],
+                        "args2": [
+                            {
+                                "visible": [True] * (len(fig.data) - num_overlay_traces)
+                                + [False] * num_overlay_traces,
+                            },
+                            {
+                                "title": {
+                                    "text": "TAS Diagram",
+                                    "x": 0.5,
+                                }
+                            },
+                        ],
+                    },
+                ],
+                "showactive": True,
+                "x": 0.0,
+                "y": -0.15,
+                "xanchor": "left",
+                "yanchor": "top",
+            }
+        ],
+        margin={"t": 120},
+    )
+    return fig
---
+++ src/geoplotnik/components/tas_diagram/rocks.py
@@ -0,0 +1,124 @@
+"""Definitions of the borders of different rock kinds on the TAS diagram."""
+
+from dataclasses import dataclass
+from enum import Enum
+
+import plotly.graph_objects as go
+
+
+@dataclass
+class TasBorder:
+    name: str
+    xs: list[float]
+    ys: list[float]
+    colour: str = "black"
+    label_position: tuple[float, float] | None = None
+    visible: bool = True
+
+    def to_scatter(self) -> go.Scatter:
+        return go.Scatter(
+            x=self.xs,
+            y=self.ys,
+            mode="lines",
+            name=self.name,
+            visible=self.visible,
+            showlegend=False,
+            line={"color": self.colour, "width": 1.5},
+            hovertemplate="x: %{x}<br>y: %{y}<extra></extra>",
+        )
+
+    def is_closed_polygon(self) -> bool:
+        """Check whether this is a closed polygon or an open polygon."""
+        return self.xs[0] == self.xs[-1] and self.ys[0] == self.ys[-1]
+
+    def clean_duplicates(self) -> tuple[list[float], list[float]]:
+        if self.is_closed_polygon():
+            return self.xs[:-1], self.ys[:-1]
+        else:
+            return self.xs, self.ys
+
+    def to_label(self) -> go.Scatter:
+        if self.label_position is None:
+            xs, ys = self.clean_duplicates()
+
+            cx = sum(xs) / len(xs)
+            cy = sum(ys) / len(ys)
+        else:
+            cx = self.label_position[0]
+            cy = self.label_position[1]
+
+        return go.Scatter(
+            x=[cx],
+            y=[cy],
+            text=[self.name],
+            mode="text",
+            showlegend=False,
+            visible=self.visible,
+            textfont={"color": "rgba(0, 0, 0, 0.6)", "size": 10},
+            hoverinfo="skip",
+        )
+
+
+# TODO: Not all borders are specified exactly. They are commented out.
+class Rocks(Enum):
+    """Collection of border coordinates of the different rocks in a TAS diagram."""
+
+    Andesite = TasBorder(name="Andesite", xs=[57, 57, 63, 63], ys=[0, 5.9, 7, 0])
+    Basalt = TasBorder(name="Basalt", xs=[45, 45, 52, 52], ys=[0, 5, 5, 0])
+    BasalticAndesite = TasBorder(
+        name="Basaltic<br>Andesite", xs=[52, 52, 57, 57], ys=[0, 5, 5.9, 0]
+    )
+    BasalticTrachyandesite = TasBorder(
+        name="Basaltic<br>Trachyandesite",
+        xs=[52, 49.4, 53, 57, 52],
+        ys=[5, 7.3, 9.3, 5.9, 5],
+    )
+    BasaniteTephrite = TasBorder(
+        name="Basanite (ol<10%)<br>Tephrite (ol>10%)",
+        xs=[41, 41, 45, 49.4, 45, 45, 41],
+        ys=[3, 7, 9.4, 7.3, 5, 3, 3],
+        label_position=(45, 7),
+    )
+    Dacite = TasBorder(name="Dacite", xs=[63, 63, 69, 77.3], ys=[0, 7, 8, 0])
+    Foidite = TasBorder(
+        name="Foidite",
+        xs=[41, 41, 45, 48.4, 52.5, 50],
+        ys=[3, 7, 9.4, 11.5, 14, 15],
+        label_position=(39, 7.5),
+    )
+    Picrobasalt = TasBorder(name="Picrobasalt", xs=[41, 41, 45, 45], ys=[0, 3, 3, 0])
+    Phonolite = TasBorder(
+        name="Phonolite",
+        xs=[52.5, 57.6, 61],
+        ys=[14, 11.7, 13.5],
+    )
+    Phonotephrite = TasBorder(
+        name="Phonotephrite",
+        xs=[49.4, 45, 48.4, 53, 49.4],
+        ys=[7.3, 9.4, 11.5, 9.3, 7.3],
+    )
+    Rhyolite = TasBorder(
+        name="Rhyolite", xs=[69, 69, 77.3], ys=[13.0, 8, 0], label_position=(73, 7.5)
+    )
+    Trachyandesite = TasBorder(
+        name="Trachyandesite", xs=[57, 53, 57.6, 63, 57], ys=[5.9, 9.3, 11.7, 7, 5.9]
+    )
+    Trachybasalt = TasBorder(
+        name="Trachybasalt", xs=[45, 49.4, 52, 45], ys=[5, 7.3, 5, 5]
+    )
+    TrachyteTrachydacite = TasBorder(
+        name="Trachyte (q<20%)<br>Trachydacite (q>20%)",
+        xs=[61, 57.6, 63, 69, 69],
+        ys=[13.5, 11.7, 7, 8, 13.5],
+    )
+    Tephriphonolite = TasBorder(
+        name="Tephriphonolite",
+        xs=[53, 48.4, 52.5, 57.6, 53],
+        ys=[9.3, 11.5, 14, 11.7, 9.3],
+    )
+
+    @classmethod
+    def to_overlay_traces(cls) -> list[go.Scatter]:
+        """Convert the rock kinds into an overlay collection."""
+
+        return [(member.value.to_scatter(), member.value.to_label()) for member in cls]
